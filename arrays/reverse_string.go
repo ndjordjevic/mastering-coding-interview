94bc445487875be2763bffe24036a8fc26a7fe90
ndjordjevic <ndjordjevic@gmail.com>
2022-03-17 08:33:08 +0100

    reverse a sting alg

---
+++ arrays/reverse_string.go
@@ -0,0 +1,12 @@
+package arrays
+
+func reverseString(str string) string {
+	runes := []rune(str)
+	n := len(runes)
+
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	}
+
+	return string(runes)
+}
---
+++ arrays/reverse_string_test.go
@@ -0,0 +1,36 @@
+package arrays
+
+import "testing"
+
+func Test_reverseString(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Should reverse a string Nenad -> daneN",
+			args: args{
+				str: "Nenad",
+			},
+			want: "daneN",
+		},
+		{
+			name: "Should reverse a string Hello world -> dlrow olleH",
+			args: args{
+				str: "Hello world",
+			},
+			want: "dlrow olleH",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseString(tt.args.str); got != tt.want {
+				t.Errorf("reverseString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
